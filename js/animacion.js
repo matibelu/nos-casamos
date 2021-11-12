3ec70eacaa41124258b6c54e02e9e7e24263f0f9
mgonzalez <gonzalez.matias.ok@gmail.com>
2021-11-11 21:46:08 -0300

    Nos casamos

---
+++ js/animacion.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(document.getElementById('firulette'));
+    let firulete = document.getElementById('firulette');
+    firulete.className = 'visible';
+    
+});
